0e69a2878948d02c06662b93a6a71d06fd7b2fb2
Domicio Jose <testedomicio@hotmail.com>
2022-01-31 17:26:23 -0300

    aula de repitição

---
+++ src/repet.java
@@ -0,0 +1,14 @@
+
+public class repet {
+
+	public static void main(String[] args) {
+		int cc = 0;
+		do {
+			System.out.println(cc);
+			cc++;
+		} while (cc< 4);
+	}
+
+}
+
+/* repetition class */

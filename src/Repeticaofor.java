11c3b1ebad8c03300a35a9e2c3ce33dbcc7a647c
Domicio Jose <testedomicio@hotmail.com>
2022-01-31 23:26:57 -0300

    aula de repetição for

---
+++ src/Repeticaofor.java
@@ -0,0 +1,13 @@
+
+public class Repeticaofor {
+
+	public static void main(String[] args) {
+		//for (int cc=0; cc<=3; cc++) {
+		//for (int cc=5; cc<=15; cc+=2) {
+		for (int cc=15; cc>=5; cc-=2) {
+			System.out.println(cc); 
+		}
+
+	}
+
+}

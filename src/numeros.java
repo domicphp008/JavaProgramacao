68c828dbe6679a29f8a699e2f16fa11ffe05b0d0
Domicio Jose <testedomicio@hotmail.com>
2022-02-02 10:20:06 -0300

    modificado numeros

--- src/numeros.java
+++ src/numeros.java
@@ -5,14 +5,15 @@ public class numeros {
 	public static void main(String[] args) {
 		int n, s=0;
 		String resp;
-		Scanner teclado = new Scanner(System.in);
-		do {
-			System.out.print("Digite um numero: ");
-			n= teclado.nextInt();
-			s += n;
-			System.out.print("Quer continuar? [S/N] ");
+		try (Scanner teclado = new Scanner(System.in)) {
+			do {
+				System.out.print("Digite um numero: ");
+				n= teclado.nextInt();
+				s += n;
+				System.out.print("Quer continuar? [S/N] ");
 ;			resp = teclado.next();
-		} while (resp.equals("S"));
+			} while (resp.equals("S"));
+		}
 		System.out.println("A soma de todos os valores é " + s);
 	}
 
